63a84c25c917809949a5fefec0ff521b0a0e6586
KornkaweeSY <137201228+KornkaweeSY@users.noreply.github.com>
2024-10-25 21:56:50 +0700

    ีupdate: index

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
     <div 
         class="max-w-full h-screen bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900"
         @mousemove="updateBackground" ref="backgroundDiv">
-        <div 
-            class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute" 
+        <div
+            class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
             :style="backgroundStyle"
         ></div>
         <div class="flex justify-between">
@@ -14,6 +14,11 @@
                     <p class="text-slate-200 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. In temporibus laboriosam consequuntur pariatur reiciendis quia consectetur obcaecati! Similique atque non illo explicabo eligendi tempore. Magni accusamus laborum distinctio vel autem.</p>
                 </div>
             </header>
+            <div class="w-1/2">
+                <div>
+                    <h1></h1>
+                </div>
+            </div>
         </div>
     </div>
 </template>
